7af20da8a4fb6b8797310a91729fa498f0657f50
Philipp <Philipp.Hofmann2@stud.hs-coburg.de>
2026-04-10 05:05:32 +0200

    initialize MiniDev project with Spring Boot and Maven setup

---
+++ src/main/java/org/philipp/fun/minidev/MiniDevApplication.java
@@ -0,0 +1,13 @@
+package org.philipp.fun.minidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/philipp/fun/minidev/MiniDevApplicationTests.java
@@ -0,0 +1,13 @@
+package org.philipp.fun.minidev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
