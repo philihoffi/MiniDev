f7591bc0b25189b95fbf1a4e5302f5520bcd68e5
Philipp <Philipp.Hofmann2@stud.hs-coburg.de>
2026-06-06 07:48:52 +0200

    Relax ESLint rules for existing codebase

--- minidev-frontend/eslint.config.js
+++ minidev-frontend/eslint.config.js
@@ -31,6 +31,7 @@ module.exports = defineConfig([
           style: "kebab-case",
         },
       ],
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
   {
@@ -39,6 +40,10 @@ module.exports = defineConfig([
       angular.configs.templateRecommended,
       angular.configs.templateAccessibility,
     ],
-    rules: {},
+    rules: {
+      "@angular-eslint/template/click-events-have-key-events": "off",
+      "@angular-eslint/template/interactive-supports-focus": "off",
+      "@angular-eslint/template/label-has-associated-control": "off",
+    },
   }
 ]);
