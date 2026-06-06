c1224d37d88347a56e0b8936552f0a3faeadbb60
Philipp <Philipp.Hofmann2@stud.hs-coburg.de>
2026-06-06 07:55:04 +0200

    Fix MainLayoutComponent test: add ActivatedRoute provider

--- minidev-frontend/src/app/components/layout/main-layout/main-layout.component.spec.ts
+++ minidev-frontend/src/app/components/layout/main-layout/main-layout.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 import { MainLayoutComponent } from './main-layout.component';
 
 describe('MainLayoutComponent', () => {
@@ -9,7 +10,7 @@ describe('MainLayoutComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MainLayoutComponent],
-      providers: [provideHttpClient()]
+      providers: [provideHttpClient(), provideRouter([])]
     })
     .compileComponents();
 
